c4fbd37eee97f28881e1d0b8dc6033ee34ebebd1
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-01-20 10:29:16 -0500

    cc13x2: add cc13x2_rf module.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

---
+++ cpu/cc13x2/radio/rf_internal.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020 Locha Inc
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         cpu_cc13x2_radio
+ * @{
+ *
+ * @file
+ * @brief           CC13x2 radio
+ *
+ * @author          Jean Pierre Dudey <jeandudey@hotmail.com
+ * @}
+ */
