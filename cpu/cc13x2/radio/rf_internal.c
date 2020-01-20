98655fd3227b739c253645363abe571f0d08051c
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-01-20 10:51:22 -0500

    cc13x2: add CPE ISR handlers.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- cpu/cc13x2/radio/rf_internal.c
+++ cpu/cc13x2/radio/rf_internal.c
@@ -1,9 +1,9 @@
 /*
  * Copyright (C) 2020 Locha Inc
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 /**
@@ -13,6 +13,26 @@
  * @file
  * @brief           CC13x2 radio
  *
- * @author          Jean Pierre Dudey <jeandudey@hotmail.com
+ * @author          Jean Pierre Dudey <jeandudey@hotmail.com>
  * @}
  */
+
+#include "cpu.h"
+
+#define RF_BOOT0          (0x00000000)
+
+void isr_rfc_cpe1(void)
+{
+}
+
+void isr_rfc_cpe0(void)
+{
+}
+
+void isr_rfc_hw(void)
+{
+}
+
+void isr_rfc_cmd_ack(void)
+{
+}
